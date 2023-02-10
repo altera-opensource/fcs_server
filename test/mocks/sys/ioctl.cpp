509755dcd1be98cb9418fbd69f61ccbbcfbc35b2
mlatkows <71813615+mlatkows@users.noreply.github.com>
2023-02-10 10:45:55 +0100

    Release 1.1.88.5 (#3)

--- test/mocks/sys/ioctl.cpp
+++ test/mocks/sys/ioctl.cpp
@@ -46,97 +46,71 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define CHIPID_LOW 0x18ACEC5A
 #define CHIPID_HIGH 0x0782C6CC
 
-int ioctl (int fileDescriptor, unsigned long int commandCode, intel_fcs_dev_ioctl *data)
-{
+int ioctl(int fileDescriptor, unsigned long int commandCode, intel_fcs_dev_ioctl *data) {
     Logger::log("ioctl() mock called", Debug);
-    if (data == nullptr)
-    {
+    if (data == nullptr) {
         errno = EFAULT;
         return -1;
     }
-    if (fileDescriptor != MOCK_FILE_DESCRIPTOR)
-    {
+    if (fileDescriptor != MOCK_FILE_DESCRIPTOR) {
         errno = EBADF;
         return -1;
     }
-    switch (commandCode)
-    {
-        case (INTEL_FCS_DEV_CHIP_ID_CMD):
-        {
+    switch (commandCode) {
+        case (INTEL_FCS_DEV_CHIP_ID_CMD): {
             data->com_paras.c_id.chip_id_high = CHIPID_HIGH;
             data->com_paras.c_id.chip_id_low = CHIPID_LOW;
             data->status = 0;
         }
         break;
-        case (INTEL_FCS_DEV_PSGSIGMA_TEARDOWN_CMD):
-        {
-            if (data->com_paras.tdown.sid != FcsSimulator::expectedSessionId)
-            {
+        case (INTEL_FCS_DEV_PSGSIGMA_TEARDOWN_CMD): {
+            if (data->com_paras.tdown.sid != FcsSimulator::expectedSessionId) {
                 data->status = -1;
-            }
-            else
-            {
+            } else {
                 data->status = 0;
             }
         }
         break;
-        case (INTEL_FCS_DEV_ATTESTATION_SUBKEY_CMD):
-        {
-            if (data->com_paras.subkey.rsp_data_sz < ATTESTATION_SUBKEY_RSP_MAX_SZ)
-            {
+        case (INTEL_FCS_DEV_ATTESTATION_SUBKEY_CMD): {
+            if (data->com_paras.subkey.rsp_data_sz < ATTESTATION_SUBKEY_RSP_MAX_SZ) {
                 errno = EINVAL;
                 return -1;
             }
-            if (data->com_paras.subkey.cmd_data_sz != FcsSimulator::expectedCreateSubkeyCommandLength)
-            {
+            if (data->com_paras.subkey.cmd_data_sz != FcsSimulator::expectedCreateSubkeyCommandLength) {
                 data->status = -1;
-            }
-            else
-            {
-                std::vector<uint8_t> buffer(ATTESTATION_SUBKEY_RSP_MAX_SZ, 0x7E); //0111 1110
+            } else {
+                std::vector <uint8_t> buffer(ATTESTATION_SUBKEY_RSP_MAX_SZ, 0x7E); //0111 1110
                 std::copy(buffer.begin(), buffer.end(), data->com_paras.subkey.rsp_data);
                 data->com_paras.subkey.rsp_data_sz = buffer.size();
                 data->status = 0;
             }
         }
         break;
-        case (INTEL_FCS_DEV_ATTESTATION_MEASUREMENT_CMD):
-        {
-            if (data->com_paras.measurement.rsp_data_sz < ATTESTATION_MEASUREMENT_RSP_MAX_SZ)
-            {
+        case (INTEL_FCS_DEV_ATTESTATION_MEASUREMENT_CMD): {
+            if (data->com_paras.measurement.rsp_data_sz < ATTESTATION_MEASUREMENT_RSP_MAX_SZ) {
                 errno = EINVAL;
                 return -1;
             }
-            if (data->com_paras.measurement.cmd_data_sz != FcsSimulator::expectedGetMeasurementCommandLength)
-            {
+            if (data->com_paras.measurement.cmd_data_sz != FcsSimulator::expectedGetMeasurementCommandLength) {
                 data->status = -1;
             }
-            std::vector<uint8_t> buffer(FcsSimulator::expectedGetMeasurementResponseLength , 0x7E); //0111 1110
+            std::vector <uint8_t> buffer(FcsSimulator::expectedGetMeasurementResponseLength, 0x7E); //0111 1110
             std::copy(buffer.begin(), buffer.end(), data->com_paras.subkey.rsp_data);
             data->com_paras.measurement.rsp_data_sz = buffer.size();
             data->status = 0;
         }
         break;
-        case (INTEL_FCS_DEV_ATTESTATION_GET_CERTIFICATE):
-        {
-            if (data->com_paras.certificate.rsp_data_sz < ATTESTATION_CERTIFICATE_RSP_MAX_SZ)
-            {
-                errno = EINVAL;
-                return -1;
-            }
-            if (data->com_paras.certificate.c_request != (int)FcsSimulator::expectedCertificateRequest)
-            {
-                data->status = -1;
-            }
-            std::vector<uint8_t> buffer(FcsSimulator::expectedGetAttCertResponseLength , 0x7E); //0111 1110
-            std::copy(buffer.begin(), buffer.end(), data->com_paras.certificate.rsp_data);
-            data->com_paras.certificate.rsp_data_sz = buffer.size();
+#ifdef SPDM_SIM
+        case (INTEL_FCS_DEV_ATTESTATION_GET_CERTIFICATE): {
+            std::vector<uint8_t> outputBuffer;
+            int status = SpdmSimulator::sendGetAttestationCommand(data->com_paras.certificate.c_request, outputBuffer);
+            Logger::logWithReturnCode("SpdmSimulator::sendGetAttestationCommand called", status, Debug);
+            std::copy(outputBuffer.begin(), outputBuffer.end(), static_cast<char*>(data->com_paras.certificate.rsp_data));
+            data->com_paras.certificate.rsp_data_sz = outputBuffer.size();
             data->status = 0;
         }
         break;
-#ifdef SPDM_SIM
-        case (INTEL_FCS_DEV_MBOX_SEND):
-        {
+        case (INTEL_FCS_DEV_MBOX_SEND): {
             std::vector<uint8_t> inputBuffer;
             uint8_t* dataPtr = static_cast<uint8_t*>(data->com_paras.mbox_send_cmd.cmd_data);
             inputBuffer.assign(dataPtr, dataPtr + data->com_paras.mbox_send_cmd.cmd_data_sz);
@@ -148,9 +122,23 @@ int ioctl (int fileDescriptor, unsigned long int commandCode, intel_fcs_dev_ioct
             data->status = 0;
         }
         break;
+#else
+        case (INTEL_FCS_DEV_ATTESTATION_GET_CERTIFICATE): {
+            if (data->com_paras.certificate.rsp_data_sz < ATTESTATION_CERTIFICATE_RSP_MAX_SZ) {
+                errno = EINVAL;
+                return -1;
+            }
+            if (data->com_paras.certificate.c_request != (int) FcsSimulator::expectedCertificateRequest) {
+                data->status = -1;
+            }
+            std::vector <uint8_t> buffer(FcsSimulator::expectedGetAttCertResponseLength, 0x7E); //0111 1110
+            std::copy(buffer.begin(), buffer.end(), data->com_paras.certificate.rsp_data);
+            data->com_paras.certificate.rsp_data_sz = buffer.size();
+            data->status = 0;
+        }
+        break;
 #endif
-        default:
-        {
+        default: {
             errno = EINVAL;
             return -1;
         }
